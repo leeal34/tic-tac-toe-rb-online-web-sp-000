c141872e9568bd47ca5b3e45713425dd0f809ade
Alexander Lee <leeal34uw@gmail.com>
2021-05-20 02:02:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,6 +38,10 @@ def position_taken?(board, index)
   board[index] == 'X' || board[index] == 'O'
 end
 
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
 
   
 
